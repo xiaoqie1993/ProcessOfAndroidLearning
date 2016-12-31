f7e28decb79fb45379580203043da8dbd1d5c6a0
xiaoqie1993 <907508273@qq.com>
2016-12-31 15:40:58 +0800

    接入QQ

--- app/src/main/java/com/ustcxiaoqie/learn/processoflearning/tools/Constant.java
+++ app/src/main/java/com/ustcxiaoqie/learn/processoflearning/tools/Constant.java
@@ -11,5 +11,6 @@ public class Constant {
     public final static String getUrl0 = "http://api.openweathermap.org/data/2.5/weather?&appid=f24a08d984e87e536ac6595b4e841819?id=";
     public final static String getUrl = "http://www.weather.com.cn/data/cityinfo/101010100.html";
     public final static String postUrl = "http://api.openweathermap.org/data/2.5/weather";
+    public final static String APP_ID = "101372267";
 
 }
