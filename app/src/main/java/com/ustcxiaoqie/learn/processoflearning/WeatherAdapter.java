4f213921f23a5b31ce709728d9e3087a2e8708d8
Bryant <907508273@qq.com>
2017-01-01 21:53:17 +0800

    完善天气模块

---
+++ app/src/main/java/com/ustcxiaoqie/learn/processoflearning/WeatherAdapter.java
@@ -0,0 +1,36 @@
+package com.ustcxiaoqie.learn.processoflearning;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by Xiaoqie on 2017/1/1.
+ */
+
+public class WeatherAdapter extends BaseAdapter {
+    private Context mContext;
+    public WeatherAdapter(Context context){
+        this.mContext = context;
+    }
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
